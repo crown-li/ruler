cc1b80ec8b26958b2365742fce77cd63cc589695
crown <240874144@qq.com>
2018-06-10 23:28:51 +0800

    修改部分bug

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,23 +1,19 @@
 let app = getApp();
 const ctx = wx.createCanvasContext('canvas');
-
-
-
-
 Page({
 
   data: {
-    canvasHeight: 50,
-    currentVal: 0,
-    ratio: 10,
+    canvasHeight: 152,
+    currentVal: 30,//当前值
+    minCurrentVal:30,//最小值
+    maxCurrentVal:120,//最大值
+    ratio: 10
   },
 
   onLoad: function (options) {
     let _this = this;
     _this.getSystemInfo();
-
     _this.drawRuler();
-
   },
 
   //获取系统信息
@@ -25,11 +21,11 @@ Page({
     let _this = this;
     wx.getSystemInfo({
       success: function (res) {
-        let screenWidth = res.windowWidth;
+        let canvasWidth = res.windowWidth;
         let screenHeight = res.windowHeight;
         let pixelRatio = res.pixelRatio;
         _this.setData({
-          screenWidth,
+          canvasWidth,
           screenHeight,
           pixelRatio
         })
@@ -37,14 +33,14 @@ Page({
     })
   },
 
-  drawRuler: function (currentVal) {
+  drawRuler: function () {
     let _this = this;
 
     let pixelRatio = this.data.pixelRatio
-    let canvasHeight = this.data.canvasHeight;//canvas高度
+    let canvasHeight = this.data.canvasHeight - 59;//canvas高度
     let ratio = this.data.ratio;//偏移量
     let ruleOrigin = 0 //rule起点
-    let color = '0,0,0';
+    let color = '177,177,177';//渐变颜色
     let opacityRange = [
       { range: 0, color: 'rgba(' + color + ',0)' },
       { range: 0.4, color: 'rgba(' + color + ',1)' },
@@ -52,18 +48,8 @@ Page({
       { range: 1, color: 'rgba(' + color + ',0)' },
     ]
 
-    let minScale = 10;//最小刻度高度
-    let maxScale = 20;//最大刻度高度
-    let centerHei = 50;
+    let canvasWidth = _this.data.canvasWidth;
 
-    let FontSize = 12;//刻度字体大小
-
-    let canvasWidth = _this.data.screenWidth;
-    _this.setData({
-      canvasWidth
-    })
-
-    //createLinearGradient,createCircularGradient
     const grd = ctx.createLinearGradient(ruleOrigin, canvasHeight, canvasWidth, canvasHeight);
     grd.addColorStop(opacityRange[0].range, opacityRange[0].color);
     grd.addColorStop(opacityRange[1].range, opacityRange[1].color);
@@ -71,43 +57,56 @@ Page({
     grd.addColorStop(opacityRange[3].range, opacityRange[3].color);
     ctx.setStrokeStyle(grd)
 
-    currentVal = currentVal ? currentVal : 0;
+    let currentVal = _this.data.currentVal * 10;
+
+    //误差修正
+    if (pixelRatio == 2) {
+      pixelRatio = 2
+    } else if (pixelRatio == 2.75) {
+      pixelRatio = 4
+    }
 
     let centerPoint = Math.ceil(canvasWidth / 2 + pixelRatio);//中心点
     let preScale = Math.ceil(centerPoint / ratio);//中心点左边平均刻度份数
     let maxVal = Math.ceil(currentVal - centerPoint) + canvasWidth //最大刻度
     let minVal = Math.ceil(currentVal - preScale);//最小刻度
 
-
     ctx.setLineWidth(1)
-
-
-    //尺
-    ctx.beginPath();
-    ctx.moveTo(ruleOrigin, canvasHeight);
-    ctx.lineTo(canvasWidth, canvasHeight);
-    ctx.stroke();
-    ctx.closePath();
+    ctx.setTextAlign('center')
 
     //中间线
     ctx.beginPath();
-    ctx.moveTo(centerPoint, canvasHeight);
-    ctx.lineTo(centerPoint, canvasHeight - centerHei);
+    ctx.setFontSize(36)
+    ctx.setFillStyle('#50D0C6')
+    let metrics = ctx.measureText(this.data.currentVal);
+    ctx.fillText(this.data.currentVal, centerPoint, 30);
+    ctx.setFontSize(14)
+    ctx.fillText('kg', centerPoint + 15 + metrics.width / 2, 30);
+    ctx.setStrokeStyle('#FC6496')
+    ctx.moveTo(centerPoint, 59);
+    ctx.lineTo(centerPoint, 152);
     ctx.stroke();
     ctx.closePath();
 
     //刻度值
     ctx.beginPath();
-    ctx.setFontSize(FontSize);
-    ctx.setTextAlign('center')
+    ctx.setFontSize(12);
+    ctx.setStrokeStyle(grd)
+    ctx.setFillStyle('rgb(177,177,177)')
     let n = 0;
     let drawX = '';
-    console.log('minVal', minVal)
     for (let i = minVal; i <= maxVal; i++) {
-      drawX = n * ratio + ruleOrigin
-      i % ratio == 0 ? ctx.fillText(i > 0 ? i : '', i >= 0 ? drawX : '', canvasHeight - maxScale - 5) : '';
-      ctx.moveTo(i > 0 ? drawX : '', canvasHeight);
-      ctx.lineTo(i > 0 ? drawX : '', i % ratio == 0 ? canvasHeight - maxScale : canvasHeight - minScale);
+      drawX = n.toFixed(1) * ratio + ruleOrigin
+      if (i % ratio == 0) {
+        ctx.fillText(i > 30 ? i / ratio : '', i >= 30 ? drawX : '', 80)
+        ctx.moveTo(i > 30 ? drawX : '', 86);
+        ctx.lineTo(i > 30 ? drawX : '', 132);
+      } else {
+        if (i % 2 == 0) {
+          ctx.moveTo(i > 30 ? drawX : '', 97);
+          ctx.lineTo(i > 30 ? drawX : '', 122);
+        }
+      }
       n++
     }
     ctx.stroke();
@@ -115,31 +114,33 @@ Page({
 
     ctx.draw();
 
-  },
+    },
 
-  _pageX: 0,
-  start: function (e) {
-    this._pageX = e.touches[0].x;
-  },
+    touchStart: 0,
+      start: function (e) {
+        this.touchStart = e.touches[0].x;
+        this.data.touchEnd = this.data.currentVal;//记录鼠标按下时当前的值
+      },
 
-  _pageCount: 0,
-  move: function (e) {
-    let min = e.touches[0].x - this._pageX
-    let currentVal = Math.ceil(this.data.currentVal + min)
+    move: function (e) {
+      let min = e.touches[0].x - this.touchStart
+      let currentVal = (Number(this.data.touchEnd * 10) + Number(-min)).toFixed(1);
 
-    if (currentVal > 0) {
-      return
-    }
+      let minCurrentVal = this.data.minCurrentVal * 10;
+      let maxCurrentVal = this.data.maxCurrentVal * 10;
 
-    let scale = -Math.ceil(currentVal / 100);
+      if (currentVal < minCurrentVal) {
+        currentVal = minCurrentVal
+      }
+      if (currentVal > maxCurrentVal) {
+        currentVal = maxCurrentVal
+      }
 
-    this.setData({
-      currentVal,
-      scale
-    })
-
-    this.drawRuler(scale)
-  }
-
+      currentVal = currentVal / 10
+      this.setData({
+        currentVal
+      })
+      this.drawRuler()
+    },
 
 });
